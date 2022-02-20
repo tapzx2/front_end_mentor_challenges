925648a396cdfcc1934a2227569d7c2fa960e348
Nicholas Tapia <tapia.nicholas@gmail.com>
2022-02-20 12:44:27 -0500

    add js email validation

--- pod-request-access-landing-page/script.js
+++ pod-request-access-landing-page/script.js
@@ -3,10 +3,17 @@ console.log("it works");
 const form = document.querySelector(".form");
 const formInput = document.querySelector(".form-input");
 const submit = document.querySelector(".request");
+var mailformat =
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+/* using html5 spec https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript */
 
 submit.onclick = function () {
-  formInput.classList.add("invalid");
-  form.classList.add("message");
+  if (formInput.value.match(mailformat)) {
+    /* do nothing */
+  } else {
+    formInput.classList.add("invalid");
+    form.classList.add("message");
+  }
 };
 
 document.addEventListener("keydown", function (event) {
