db8438bb99caaf1d4351c8a64f39a428f089b256
Nicholas Tapia <tapia.nicholas@gmail.com>
2022-02-20 09:23:00 -0500

    add js form validiation

--- pod-request-access-landing-page/script.js
+++ pod-request-access-landing-page/script.js
@@ -1,1 +1,14 @@
-script.js;
+console.log("it works");
+
+const formInput = document.querySelector(".form-input");
+const submit = document.querySelector(".request");
+
+submit.onclick = function () {
+  formInput.classList.add("invalid");
+};
+
+document.addEventListener("keydown", function (event) {
+  if (event.target.matches(".form-input")) {
+    formInput.classList.remove("invalid");
+  }
+});
