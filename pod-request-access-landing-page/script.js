239a738431e31008c36ea9ad230e627a4d4e5b10
Nicholas Tapia <tapia.nicholas@gmail.com>
2022-02-20 12:02:09 -0500

    before refactor

--- pod-request-access-landing-page/script.js
+++ pod-request-access-landing-page/script.js
@@ -1,14 +1,17 @@
 console.log("it works");
 
+const form = document.querySelector(".form");
 const formInput = document.querySelector(".form-input");
 const submit = document.querySelector(".request");
 
 submit.onclick = function () {
   formInput.classList.add("invalid");
+  form.classList.add("message");
 };
 
 document.addEventListener("keydown", function (event) {
-  if (event.target.matches(".form-input")) {
+  if (event.target.matches(".form-input") || event.target.matches(".form")) {
     formInput.classList.remove("invalid");
+    form.classList.remove("message");
   }
 });
