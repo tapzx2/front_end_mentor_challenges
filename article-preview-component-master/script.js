e60aeebb0b172b14466ebdf84d182d923e189081
Nicholas Tapia <tapzx2@Nicholass-MacBook-Air.local>
2022-03-01 20:07:35 -0500

    refactor

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -2,39 +2,31 @@ console.log('it works')
 
 const authorCard = document.querySelector('.author-card')
 const shareCard = document.querySelector('.share-card')
+shareCard.style.display = 'none'
 
-function showShare() {
-    if (shareCard.classList.contains('toggle-off')) {
-        authorCard.classList.add('toggle-off')
-        shareCard.classList.remove('toggle-off')
-        console.log('showing share info')
-    }
-}
 
-function showAuthor() {
-    if (authorCard.classList.contains('toggle-off')) {
-        shareCard.classList.add('toggle-off')
-        authorCard.classList.remove('toggle-off')
-        console.log('showing author info')
+function toggleShare() {
+    if (shareCard.style.display === 'none') {
+        shareCard.style.display = 'flex'
+    } else {
+        shareCard.style.display = 'none'
     }
 }
 
 window.addEventListener('click', function(event) {
-    if ((event.target.matches('.share-card')) ||
+    if ((event.target.matches('button.toggle-share')
+    || (event.target.matches('img.toggle-share'))
+    )) {
+        console.log('toggled clicked')
+        toggleShare()
+    }
+    else if ((event.target.matches('.share-card')) ||
     (event.target.matches('.share-info'))
     ) {
         console.log('do nothing')
-    } else {
-        showAuthor()
+    } else if (shareCard.style.display === 'flex') {
+        toggleShare()
     }
 })
-
-window.addEventListener('click', function(event) {
-    if ((event.target.matches('.show-share') )) {
-        console.log('clicked')
-        showShare()
-    }
-})
-
 
 
