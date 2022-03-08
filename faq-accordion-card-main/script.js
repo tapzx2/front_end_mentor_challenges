6522d18a5950e669076c04566f3536166e634f07
Nicholas Tapia <tapzx2@Nicholass-MacBook-Air.local>
2022-03-08 13:31:53 -0800

    mobile draft

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -6,13 +6,19 @@ window.addEventListener("click", function (event) {
   if (event.target.matches("button")) {
     console.log("button clicked");
     console.log(event.target.attributes["aria-controls"].value);
+    const arrow = document.querySelector(`#${event.target.id} + svg`);
     const panel = document.querySelector(
       `#${event.target.attributes["aria-controls"].value}`
     );
     if (panel.classList.contains("hidden")) {
       panel.classList.remove("hidden");
+      arrow.style.transform = "rotate(180deg)";
+      event.target.attributes["aria-expanded"].value = "True";
+      event.target.classList.add("active");
     } else {
       panel.classList.add("hidden");
+      arrow.style.transform = "unset";
+      event.target.classList.remove("active");
     }
   }
 });
